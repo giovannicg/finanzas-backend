716927a96a5151c78da50da2f420e557086c05f9
giovannicg <60613860+giovannicg@users.noreply.github.com>
2026-03-18 20:51:18 -0500

    Fix root redirect: simplify to always redirect to /login

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,12 +1,5 @@
 import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
 
-export default async function Home() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("token");
-  if (token) {
-    redirect("/dashboard");
-  } else {
-    redirect("/login");
-  }
+export default function Home() {
+  redirect("/login");
 }
